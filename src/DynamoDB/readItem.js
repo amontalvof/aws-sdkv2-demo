8e1576768ee7a5eac6f0f0dbe09dcc9ad596b6c6
amontalvof <amontalvof92@gmail.com>
2023-06-12 01:10:22 -0400

    feat: Get Movie Item

---
+++ src/DynamoDB/readItem.js
@@ -0,0 +1,21 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'us-east-2' });
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+    TableName: 'Movies',
+    Key: {
+        year: 2023,
+        title: 'New movie',
+    },
+};
+
+docClient.get(params, function (err, data) {
+    if (err) {
+        console.log('unable to get movie', err);
+    } else {
+        console.log(data['Item']);
+    }
+});
